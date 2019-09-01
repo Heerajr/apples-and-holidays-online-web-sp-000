525c038cca0bce6a4e357bdcd1b75f69e4430d5f
Heeraj Ramsanai <ramsanai_heeraj@live.com>
2019-09-01 17:42:27 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -64,9 +64,8 @@ def all_supplies_in_holidays(holiday_hash)
   holiday_hash.each do |season, holiday|
     puts "#{season.capitalize}:"
     holiday.each do |holiday_name, supplies|
+      binding.pry
     end
-
-  binding.pry
   end
 end
 
